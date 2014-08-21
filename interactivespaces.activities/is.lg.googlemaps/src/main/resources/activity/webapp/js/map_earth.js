c9d5ccc863c2948b3cd95ebdce3395f8c3712d2a
Marc <marc@marc.(none)>
2014-08-21 22:02:45 +0200

    Activities

---
+++ interactivespaces.activities/is.lg.googlemaps/src/main/resources/activity/webapp/js/map_earth.js
@@ -0,0 +1,80 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+var marker_global;
+
+function map_earth() {
+ var mapOptions = {
+    center: new google.maps.LatLng(30, 0),
+    zoom: 3
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+
+	
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    //marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
+
+  var marker = new google.maps.Marker({
+    map: map,
+	title: place.name,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(14);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+	
+console.log(marker.title);
+	send_voice("search="+place.name);
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+
+}
+
+
+
+
+//google.maps.event.addDomListener(window, 'load', initialize);
